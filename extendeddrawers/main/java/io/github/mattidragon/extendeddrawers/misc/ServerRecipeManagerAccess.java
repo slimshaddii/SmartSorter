ea064a029c813c723a9b382fa7fc7d1bf5fcce70
slimshaddii <shadestrag@gmail.com>
2025-10-13 13:48:56 +0800

    Cleanup

--- extendeddrawers/main/java/io/github/mattidragon/extendeddrawers/misc/ServerRecipeManagerAccess.java
+++
@@ -1,7 +0,0 @@
-package io.github.mattidragon.extendeddrawers.misc;
-
-import net.minecraft.recipe.PreparedRecipes;
-
-public interface ServerRecipeManagerAccess {
-    PreparedRecipes getPreparedRecipes();
-}
