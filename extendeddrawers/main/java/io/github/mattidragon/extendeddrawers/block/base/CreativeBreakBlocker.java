cea21520fa3a445966c10e05d36b83541a07764b
slimshaddii <shadestrag@gmail.com>
2025-10-13 13:39:19 +0800

    Bug fixes

--- extendeddrawers/main/java/io/github/mattidragon/extendeddrawers/block/base/CreativeBreakBlocker.java
+++
@@ -1,8 +0,0 @@
-package io.github.mattidragon.extendeddrawers.block.base;
-
-import net.minecraft.block.BlockState;
-import net.minecraft.util.math.Direction;
-
-public interface CreativeBreakBlocker {
-    boolean isFront(BlockState state, Direction direction);
-}
